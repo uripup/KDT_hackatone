886d364a03a0b335c70e8100d5a21c6b70f0e1e2
황민지 <hminji4565@gmail.com>
2024-10-23 20:37:30 +0900

    스프링부트 추가 및 localhost 서버 실행

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,15 @@
  */
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
 
+@PropertySource("file:${user.home}/config/")
+@SpringBootApplication
+public class App {
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
     }
 }
