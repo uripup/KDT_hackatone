63880b45f9f002bd54ce23c83405b9e0a2e13b8f
황민지 <hminji4565@gmail.com>
2024-10-28 09:44:06 +0900

    공공데이터 파싱 후 데이터 받아 핑 찍기

--- app/src/main/java/org/example/ParsingData.java
+++ app/src/main/java/org/example/ParsingData.java
@@ -3,24 +3,21 @@ package org.example;
 import org.example.vo.PublicData;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Properties;
 
 public class ParsingData {
-    public ArrayList<PublicData> getTagValue(NodeList items) {
-        ArrayList<PublicData> list = new ArrayList<>();
+    public HashMap<String, Object> getTagValue(NodeList items) {
+        HashMap<String, Object> mapData = new HashMap<>();
 
         for (int i = 0; i < items.getLength(); i++) {
             PublicData publicData = new PublicData();
@@ -28,12 +25,29 @@ public class ParsingData {
             publicData.setWgs84Lon(item.getElementsByTagName("wgs84Lon").item(0).getTextContent());
             publicData.setWgs84Lat(item.getElementsByTagName("wgs84Lat").item(0).getTextContent());
             publicData.setName(item.getElementsByTagName("dutyName").item(0).getTextContent());
-            list.add(publicData);
+            mapData.put(String.valueOf(i), publicData);
         }
-        return list;
+        return mapData;
     }
 
-    public ArrayList<PublicData> xmlParsing(String urlKey) throws Exception {
+    public void saveData(HashMap<String, Object> dataMap) throws IOException {
+        Properties props = new Properties();
+        try (InputStream is = getClass().getResourceAsStream("application.properties")) {
+            props.load(is);
+        }
+
+        String url  = props.getProperty("spring.datasource.url");
+        String username  = props.getProperty("spring.datasource.username");
+        String password  = props.getProperty("spring.datasource.password");
+
+        System.out.println(url);
+        System.out.println(username);
+        System.out.println(password);
+
+
+    }
+
+    public HashMap<String, Object> xmlParsing(String urlKey) throws Exception {
         ArrayList<PublicData> datalist = new ArrayList<>();
 
             String id = "10000695";
@@ -79,48 +93,11 @@ public class ParsingData {
 
             NodeList items = document.getElementsByTagName("item");
 
-            datalist = getTagValue(items);
+//            saveData(getTagValue(items));
 
 //            for (PublicData data : datalist) {
 //                System.out.println(data.getWgs84Lon() + ", " + data.getWgs84Lat());
 //            }
-        return datalist;
+        return getTagValue(items);
     }
-
-//    public void jsonParsing() throws Exception {
-//        String key = "SdeVRGxeat0HFsYbWRArfdmvZr9D7A4%2FN1IDlh2HxrZcOrO4OX51OU%2FswL7U2GWnhJsKoUjz6OxrW05kc7TYlg%3D%3D";
-//
-//        String urlBuilder =
-////                "https://apis.data.go.kr/1741000/pfc2/pfc/getPfctInfo2" + // 전국 어린이 놀이시설
-//                        "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + key + /*Service Key*/
-//                        "&" + URLEncoder.encode("recordCountPerPage", "UTF-8") + "=" + URLEncoder.encode("100", "UTF-8") + /*한 페이지 결과 수*/
-//                        "&" + URLEncoder.encode("pageIndex", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"); /*페이지번호*/
-//
-//        URI uri = new URI(urlBuilder);
-//        URL url = uri.toURL();
-//
-//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//        conn.setRequestMethod("GET");  // GET 요청
-//        conn.setRequestProperty("Content-type", "application/json");  // 요청 헤더 설정
-//
-//        int responseCode = conn.getResponseCode();
-//        System.out.println("Response Code: " + responseCode);
-//
-//        if (responseCode == HttpURLConnection.HTTP_OK) {
-//            // 응답 데이터 읽기
-//            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-//            String inputLine;
-//            StringBuilder response = new StringBuilder();
-//
-//            while ((inputLine = in.readLine()) != null) {
-//                response.append(inputLine);
-//            }
-//            in.close();
-//
-//            // 응답 결과 출력
-//            System.out.println("Response: " + response.toString());
-//        } else {
-//            System.out.println("API 호출 실패: " + responseCode);
-//        }
-//    }
 }
--- app/src/main/java/org/example/controller/MapController.java
+++ app/src/main/java/org/example/controller/MapController.java
@@ -5,25 +5,47 @@ import org.example.vo.PublicData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-@RestController
+@Controller
+@SessionAttributes("data") // 변경: @RestController -> @Controller
 public class MapController {
-    @GetMapping("/map")
-    public void map(Model model, String latitude, String longitude) throws Exception {
+
+    // 데이터 파싱 및 세션에 저장 후 리다이렉트
+    @GetMapping("/map/data")
+    @ResponseBody
+    public HashMap<String, Object> getDataAndRedirect(HttpSession session) throws Exception {
+        HashMap<String, Object> data = new HashMap<>();
 
         String key = "SdeVRGxeat0HFsYbWRArfdmvZr9D7A4%2FN1IDlh2HxrZcOrO4OX51OU%2FswL7U2GWnhJsKoUjz6OxrW05kc7TYlg%3D%3D";
-
         String urlBuild1 = "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyBassInfoInqire" +
                 "?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + key +
-                "&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8") + /*한 페이지 결과 수*/
-                "&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"); /*페이지번호*/
-        ParsingData data = new ParsingData();
+                "&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8") +
+                "&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8");
 
-        model.addAttribute("list", data.xmlParsing(urlBuild1));
+        ParsingData parsingData = new ParsingData();
+        data.put("data", parsingData.xmlParsing(urlBuild1));
+
+        return data;
+    }
+
+    // 세션에 저장된 데이터를 가져와 화면에 전달
+    @GetMapping("/map")
+    public String showMap(Model model, HttpSession session) {
+        // 세션에서 데이터 가져오기
+        Map<String, Object> parsedData = (Map<String, Object>) session.getAttribute("data");
+        if (parsedData != null) {
+            model.addAttribute("parsedData", parsedData);
+        }
+
+        // map.html로 이동
+        return "map";
     }
 }
